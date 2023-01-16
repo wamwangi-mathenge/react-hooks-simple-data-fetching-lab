b61e3616a3b38414a008a6de2499f1fae96d1314
Brian <wamwangimathenge@gmail.com>
2023-01-16 16:52:28 +0300

    Use State

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
 // create your App component here
+import React, { useState } from "react";
+
+
+const App = () => {
+
+    const [imageUrl, setImageUrl] = useState(null);
+    const [isLoading, setIsLoading] = useState(true)
+
+    return ();
+}
+ 
+export default App;
